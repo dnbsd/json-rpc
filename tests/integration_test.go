2d0ce8ffa6daff248ba305e5ca9ee55da3b24597
dan <dnbsd@posteo.net>
2024-03-23 20:05:19 +0100

    WIP backup

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -4,94 +4,118 @@ import (
 	"context"
 	"github.com/dnbsd/jsonrpc"
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
 
-var ErrEmptyMessage = jsonrpc.NewError(1, "message is empty")
+func TestServiceModuleMethodCall(t *testing.T) {
+	req, err := jsonrpc.ParseRequest([]byte(`{"jsonrpc": "2.0", "method": "std.version", "id": 1}`))
+	assert.NoError(t, err)
 
-type EchoSubmodule struct{}
-
-func (m *EchoSubmodule) UpperCase(ctx context.Context, params jsonrpc.Params) (any, error) {
-	message := params.String("message")
-	if message == "" {
-		return nil, ErrEmptyMessage
-	}
-	return strings.ToUpper(message), nil
-}
-
-func (m *EchoSubmodule) Exports() map[string]jsonrpc.Method {
-	return map[string]jsonrpc.Method{
-		"upper_case": m.UpperCase,
-	}
-}
-
-func (m *EchoSubmodule) Submodules() map[string]jsonrpc.Module {
-	return nil
-}
-
-type EchoModule struct{}
-
-func (m *EchoModule) Echo(ctx context.Context, params jsonrpc.Params) (any, error) {
-	message := params.String("message")
-	if message == "" {
-		return nil, ErrEmptyMessage
-	}
-	return message, nil
-}
-
-func (m *EchoModule) Submodules() map[string]jsonrpc.Module {
-	return map[string]jsonrpc.Module{
-		"sub": &EchoSubmodule{},
-	}
-}
-
-func (m *EchoModule) Exports() map[string]jsonrpc.Method {
-	return map[string]jsonrpc.Method{
-		"echo": m.Echo,
-	}
-}
-
-func TestMethodCall(t *testing.T) {
-	rpc := jsonrpc.New()
-	rpc.Register("echo", &EchoModule{})
-	req := jsonrpc.NewRequest(1, "echo.echo", jsonrpc.Params{
-		"message": "hello world",
-	})
-	resp := rpc.Call(context.Background(), req)
+	service := buildService(t)
+	resp := service.Call(context.Background(), req)
+	assert.Equal(t, float64(1), resp.ID)
+	assert.Equal(t, float64(1), resp.Result)
 	assert.Nil(t, resp.Error)
-	assert.Equal(t, resp.Result, "hello world")
-	assert.Equal(t, req.ID, resp.ID)
 }
 
-func TestMethodCallError(t *testing.T) {
-	rpc := jsonrpc.New()
-	rpc.Register("echo", &EchoModule{})
-	req := jsonrpc.NewRequest(1, "echo.echo", jsonrpc.Params{})
-	resp := rpc.Call(context.Background(), req)
-	assert.ErrorIs(t, resp.Error, ErrEmptyMessage)
+func TestServiceModuleMethodCallUndefined(t *testing.T) {
+	req, err := jsonrpc.ParseRequest([]byte(`{"jsonrpc": "2.0", "method": "std.xxx", "id": 1}`))
+	assert.NoError(t, err)
+
+	service := buildService(t)
+	resp := service.Call(context.Background(), req)
+	assert.Equal(t, float64(1), resp.ID)
+	assert.Equal(t, -32601, resp.Error.Code)
 }
 
-func TestMethodCallUndefined(t *testing.T) {
-	rpc := jsonrpc.New()
-	rpc.Register("echo", &EchoModule{})
-	req := jsonrpc.NewRequest(1, "echo.echos", jsonrpc.Params{
-		"message": "hello world",
-	})
-	resp := rpc.Call(context.Background(), req)
-	assert.ErrorIs(t, jsonrpc.ErrMethodNotFound, resp.Error)
-	assert.Equal(t, resp.Result, nil)
-	assert.Equal(t, req.ID, resp.ID)
-}
+func TestServiceSubmoduleMethodCall(t *testing.T) {
+	req, err := jsonrpc.ParseRequest([]byte(`{"jsonrpc": "2.0", "method": "std.math.add", "params": [42, 23], "id": 1}`))
+	assert.NoError(t, err)
 
-func TestSubmoduleMethodCall(t *testing.T) {
-	rpc := jsonrpc.New()
-	rpc.Register("echo", &EchoModule{})
-	req := jsonrpc.NewRequest(1, "echo.sub.upper_case", jsonrpc.Params{
-		"message": "hello world",
-	})
-	resp := rpc.Call(context.Background(), req)
+	service := buildService(t)
+	resp := service.Call(context.Background(), req)
+	assert.Equal(t, float64(1), resp.ID)
+	assert.Equal(t, 65, resp.Result)
 	assert.Nil(t, resp.Error)
-	assert.Equal(t, resp.Result, "HELLO WORLD")
-	assert.Equal(t, req.ID, resp.ID)
+}
+
+func TestServiceSubmoduleMethodCallUndefined(t *testing.T) {
+	req, err := jsonrpc.ParseRequest([]byte(`{"jsonrpc": "2.0", "method": "std.math.subtract", "params": [42, 23], "id": 1}`))
+	assert.NoError(t, err)
+
+	service := buildService(t)
+	resp := service.Call(context.Background(), req)
+	assert.Equal(t, float64(1), resp.ID)
+	assert.Equal(t, -32601, resp.Error.Code)
+}
+
+func buildService(t *testing.T) *jsonrpc.Service {
+	builder := jsonrpc.Builder{
+		Modules: []jsonrpc.Module{
+			{
+				Name: "std",
+				Methods: []jsonrpc.Method{
+					{
+						Name: "version",
+						Handler: func(c *jsonrpc.Context) (any, error) {
+							return 1.0, nil
+						},
+					},
+				},
+				Submodules: []jsonrpc.Module{
+					{
+						Name: "math",
+						Methods: []jsonrpc.Method{
+							{
+								Name: "add",
+								Handler: func(c *jsonrpc.Context) (any, error) {
+									params, err := c.ParamsArray()
+									if err != nil {
+										return c.Error(err)
+									}
+
+									var sum int
+									for i := range params {
+										n, err := params.Number(i)
+										if err != nil {
+											return c.Error(err)
+										}
+										sum += n.Int()
+									}
+
+									return c.Result(sum)
+								},
+							},
+							{
+								Name: "divide",
+								Handler: func(c *jsonrpc.Context) (any, error) {
+									return nil, nil
+								},
+							},
+						},
+					},
+					{
+						Name: "string",
+						Methods: []jsonrpc.Method{
+							{
+								Name: "join",
+								Handler: func(c *jsonrpc.Context) (any, error) {
+									return nil, nil
+								},
+							},
+							{
+								Name: "split",
+								Handler: func(c *jsonrpc.Context) (any, error) {
+									return nil, nil
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	s, err := builder.Build()
+	assert.NoError(t, err)
+	return s
 }
